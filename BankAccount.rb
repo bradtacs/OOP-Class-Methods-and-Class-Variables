daebabee33d0821cefc96026d71806661d558c63
Zachary Viggiani <zach@bigmojo.net>
2017-04-04 14:28:14 -0400

    7 done

--- BankAccount.rb
+++ BankAccount.rb
@@ -1,42 +1,40 @@
 class BankAccount
-  # class vars
-  @@accounts = [] # accounts go here
-  @@interest_rate = 0.15
+    # class vars
+    @@accounts = [] # accounts go here
+    @@interest_rate = 0.15
 
-  attr_reader :balance,
-  def initialize
-    balance = 0
+    attr_reader :balance
+    def initialize
+      @balance = 0
+
+    end
+
+    def deposit(amount)
+      @balance = @balance + amount
+    end
+
+    def withdraw(amount)
+      if @balance - amount < 0
+        puts "insuf funds"
+      else
+        @balance = @balance - amount
+    end
+
+    def create
+      @@accounts << BankAccount.new
+    end
+
+    def gain_interest
+      @balance = @balance * 1 + @@interest_rate
+    end
+
   end
-
-  def deposit(amount)
-    @balance = @balance + amount
-  end
-
-  def withdraw(amount)
-    if @balance - amount < 0
-      puts "insuf funds"
-    else
-      @balance = @balance - amount
-  end
-
-  def create
-
-    new_accuont = BankAccount.new
-    @@accounts << new_accuont
-    put new_accuont
-  end
-
-  def gain_interest
-    @balance = @balance * (1 + @interest_rate)
-  end
-
-
 end
 
 my_account = BankAccount.new
 
 puts "My balance"
-puts my_account.balance
+puts my_account.inspect
 
 puts "Deposit 200"
 my_account.deposit(200)
