52a12a20d24905b3a7eb23e0eb96ef3ee272f666
Zachary Viggiani <zach@bigmojo.net>
2017-04-22 18:29:58 -0400

    Done

--- BankAccount.rb
+++ BankAccount.rb
@@ -1,12 +1,11 @@
 class BankAccount
     # class vars
     @@accounts = [] # accounts go here
-    @@interest_rate = 0.15
+    @@interest_rate = 1.01
 
-    attr_reader :balance
+    attr_reader :balance, :deposit, :withdraw
     def initialize
       @balance = 0
-
     end
 
     def deposit(amount)
@@ -20,30 +19,24 @@ class BankAccount
         @balance = @balance - amount
     end
 
-    def create
-      @@accounts << BankAccount.new
+    def self.create
+      @@accounts << BankAccount.new # add a new bank account into the array
     end
 
-    def gain_interest
-      @balance = @balance * 1 + @@interest_rate
+    def self.totol_funds
+      total = 0
+      @@accounts.each do |account|
+      total += account.balance
+     end
+     return total
+      end
+    end
+
+    def self.gain_interest_time # gain interstv
+      @@accounts.each do |account|
+        accounts.balance.to_i * @@interest_rate
+     end
     end
 
   end
 end
-
-my_account = BankAccount.new
-
-puts "My balance"
-puts my_account.inspect
-
-puts "Deposit 200"
-my_account.deposit(200)
-
-puts my_account.balance
-puts "Withdraw 500"
-my_account.withdraw(500)
-
-my_account.gain_interest
-
-puts "My balance"
-puts my_account.balance
